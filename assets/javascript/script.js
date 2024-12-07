3108b91c123e6906ad7edf26c21f7c38e93dc998
Douglas Mottershead <douglas.mottershead@outlook.com>
2024-12-07 01:27:43 +0000

    set style rule for html body

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,7 +3,7 @@ const characterImage = document.querySelector('#charSprite');
 
 //fetch data from API
 
-fetch('https://rickandmortyapi.com/api/character/1')
+fetch('https://rickandmortyapi.com/api/character/32')
     .then(response => response.json())
     .then(data => {
         console.log(data);
