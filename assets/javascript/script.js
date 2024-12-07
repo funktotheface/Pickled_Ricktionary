fbf428ee390fb41171b00d2b066284609d02083b
Douglas Mottershead <douglas.mottershead@outlook.com>
2024-12-07 01:04:20 +0000

    add basic api image function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,11 @@
 console.log('Hello, world!');
+const characterImage = document.querySelector('#charSprite');
+
+//fetch data from API
+
+fetch('https://rickandmortyapi.com/api/character/1')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        charSprite.src = data.image;
+    })
